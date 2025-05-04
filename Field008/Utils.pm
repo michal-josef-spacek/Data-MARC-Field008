6a1762e3f7ad1625219ea2a0db041665793ea589
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-04 21:13:44 +0200

    Add check_relief()

--- Field008/Utils.pm
+++ Field008/Utils.pm
@@ -8,7 +8,7 @@ use Error::Pure qw(err);
 use List::Util 1.33 qw(none);
 use Readonly;
 
-Readonly::Array our @EXPORT_OK => qw(check_date check_type_of_date);
+Readonly::Array our @EXPORT_OK => qw(check_date check_relief check_type_of_date);
 Readonly::Array our @TYPE_OF_DATES => qw(b c d e i k m n p q r s t u |);
 
 our $VERSION = 0.01;
@@ -35,6 +35,33 @@ sub check_date {
 	}
 	if ($self->{$key} ne '    ' && $self->{$key} =~ m/\ /ms) {
 		err "Parameter '$key' has value with space character.";
+	}
+	if ($self->{$key} ne '||||' && $self->{$key} =~ m/\|/ms) {
+		err "Parameter '$key' has value with pipe character.";
+	}
+
+	return;
+}
+
+sub check_relief {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key} || ! defined $self->{$key}) {
+		err "Parameter '$key' is required.";
+	}
+	if (ref $self->{$key} ne '') {
+		err "Parameter '$key' must be a scalar value.",
+			'Reference', (ref, $self->{$key}),
+		;
+	}
+	if (length $self->{$key} != 4) {
+		err "Parameter '$key' length is bad.",
+			'Length', (length $self->{$key}),
+			'Value', $self->{$key},
+		;
+	}
+	if ($self->{$key} !~ m/^[\ abcdefgijkmz\|]{4}$/ms) {
+		err "Parameter '$key' contain bad relief character.";
 	}
 	if ($self->{$key} ne '||||' && $self->{$key} =~ m/\|/ms) {
 		err "Parameter '$key' has value with pipe character.";
