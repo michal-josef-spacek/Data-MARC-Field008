1679d06ec8efbd36082f46f9a27d92a334f092e3
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-09 20:18:10 +0200

    Add check_target_audience()

--- Field008/Utils.pm
+++ Field008/Utils.pm
@@ -11,7 +11,7 @@ use Readonly;
 Readonly::Array our @EXPORT_OK => qw(check_date check_index
 	check_map_cartographic_material_type check_map_government_publication
 	check_map_item_form check_map_projection check_map_relief
-	check_map_special_format check_type_of_date);
+	check_map_special_format check_target_audience check_type_of_date);
 Readonly::Array our @INDEXES => qw(0 1 |);
 Readonly::Array our @MAP_CARTOGRAPHIC_MATERIAL_TYPES => qw(a b c d e f g u z |);
 Readonly::Array our @MAP_GOVERNMENT_PUBLICATIONS => (' ', 'a', 'c', 'f', 'i',
@@ -23,6 +23,8 @@ Readonly::Array our @MAP_PROJECTIONS => ('  ', 'aa', 'ab', 'ac', 'ad', 'ae',
 	'bf', 'bg', 'bh', 'bi', 'bj', 'bk', 'bl', 'bo', 'br', 'bs', 'bu', 'bz',
 	'ca', 'cb', 'cc', 'ce', 'cp', 'cu', 'cz', 'da', 'db', 'dc', 'dd', 'de',
 	'df', 'dg', 'dh', 'dl', 'zz', '||');
+Readonly::Array our @TARGET_AUDIENCES => (' ', 'a', 'b', 'c', 'd', 'e', 'f',
+	'g', 'j', '|');
 Readonly::Array our @TYPE_OF_DATES => qw(b c d e i k m n p q r s t u |);
 
 our $VERSION = 0.01;
@@ -248,6 +250,32 @@ sub check_map_special_format {
 	}
 	if ($self->{$key} ne '||' && $self->{$key} =~ m/\|/ms) {
 		err "Parameter '$key' has value with pipe character.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+sub check_target_audience {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key} || ! defined $self->{$key}) {
+		err "Parameter '$key' is required.";
+	}
+	if (ref $self->{$key} ne '') {
+		err "Parameter '$key' must be a scalar value.",
+			'Reference', (ref, $self->{$key}),
+		;
+	}
+	if (length $self->{$key} != 1) {
+		err "Parameter '$key' length is bad.",
+			'Length', (length $self->{$key}),
+			'Value', $self->{$key},
+		;
+	}
+	if (none { $self->{$key} eq $_ } @TARGET_AUDIENCES) {
+		err "Parameter '$key' has bad value.",
 			'Value', $self->{$key},
 		;
 	}
