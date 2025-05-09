0755c9aab55aa31647fac27529183c653a1f12ba
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-09 20:58:43 +0200

    Add check_computer_file_item_form()

--- Field008/Utils.pm
+++ Field008/Utils.pm
@@ -9,10 +9,12 @@ use List::Util 1.33 qw(none);
 use Readonly;
 
 Readonly::Array our @EXPORT_OK => qw(check_book_illustration
-	check_conference_publication check_date check_government_publication
-	check_index check_item_form check_map_cartographic_material_type
-	check_map_projection check_map_relief check_map_special_format
-	check_target_audience check_type_of_date);
+	check_computer_file_item_form check_conference_publication check_date
+	check_government_publication check_index check_item_form
+	check_map_cartographic_material_type check_map_projection
+	check_map_relief check_map_special_format check_target_audience
+	check_type_of_date);
+Readonly::Array our @COMPUTER_FILE_ITEM_FORMS => (' ', 'o', 'q', '|');
 Readonly::Array our @CONFERENCE_PUBLICATIONS => qw(0 1 |);
 Readonly::Array our @GOVERNMENT_PUBLICATIONS => (' ', 'a', 'c', 'f', 'i',
 	'l', 'm', 'o', 's', 'u', 'z', '|');
@@ -55,6 +57,32 @@ sub check_book_illustration {
 	}
 	if ($self->{$key} ne '||||' && $self->{$key} =~ m/\|/ms) {
 		err "Parameter '$key' has value with pipe character.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+sub check_computer_file_item_form {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key} || ! defined $self->{$key}) {
+		err "Parameter '$key' is required.";
+	}
+	if (ref $self->{$key} ne '') {
+		err "Parameter '$key' must be a scalar value.",
+			'Reference', (ref, $self->{$key}),
+		;
+	}
+	if (length $self->{$key} != 1) {
+		err "Parameter '$key' length is bad.",
+			'Length', (length $self->{$key}),
+			'Value', $self->{$key},
+		;
+	}
+	if (none { $self->{$key} eq $_ } @COMPUTER_FILE_ITEM_FORMS) {
+		err "Parameter '$key' has bad value.",
 			'Value', $self->{$key},
 		;
 	}
