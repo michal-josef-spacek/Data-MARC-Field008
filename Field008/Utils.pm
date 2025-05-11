03245e3fa88068a537aa2238a6a5665e7f8051ba
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-11 10:40:58 +0200

    Add check_book_literary_form()

--- Field008/Utils.pm
+++ Field008/Utils.pm
@@ -9,12 +9,13 @@ use List::Util 1.33 qw(none);
 use Readonly;
 
 Readonly::Array our @EXPORT_OK => qw(check_book_biography check_book_festschrift
-	check_book_illustration check_computer_file_item_form
+	check_book_illustration check_book_literary_form check_computer_file_item_form
 	check_computer_file_type check_conference_publication check_date
 	check_government_publication check_index check_item_form
 	check_map_cartographic_material_type check_map_projection check_map_relief
 	check_map_special_format check_target_audience check_type_of_date);
 Readonly::Array our @BIOGRAPHIES => (' ', 'a', 'b', 'c', 'd', '|');
+Readonly::Array our @BOOK_LITERARY_FORMS => qw(0 1 d e f h i j m p s u |);
 Readonly::Array our @COMPUTER_FILE_ITEM_FORMS => (' ', 'o', 'q', '|');
 Readonly::Array our @COMPUTER_FILE_TYPE => qw(a b c d e f g h i j m u z |);
 Readonly::Array our @CONFERENCE_PUBLICATIONS => qw(0 1 |);
@@ -112,6 +113,32 @@ sub check_book_illustration {
 	}
 	if ($self->{$key} ne '||||' && $self->{$key} =~ m/\|/ms) {
 		err "Parameter '$key' has value with pipe character.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+sub check_book_literary_form {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key} || ! defined $self->{$key}) {
+		err "Parameter '$key' is required.";
+	}
+	if (ref $self->{$key} ne '') {
+		err "Parameter '$key' must be a scalar value.",
+			'Reference', (ref, $self->{$key}),
+		;
+	}
+	if (length $self->{$key} != 1) {
+		err "Parameter '$key' length is bad.",
+			'Length', (length $self->{$key}),
+			'Value', $self->{$key},
+		;
+	}
+	if (none { $self->{$key} eq $_ } @BOOK_LITERARY_FORMS) {
+		err "Parameter '$key' has bad value.",
 			'Value', $self->{$key},
 		;
 	}
