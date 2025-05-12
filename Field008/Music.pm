fc55634872fdd7cb6cf30c8d5bf9be684077cc7c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-12 23:39:57 +0200

    Use check_music_composition_form()

--- Field008/Music.pm
+++ Field008/Music.pm
@@ -3,7 +3,8 @@ package Data::MARC::Field008::Music;
 use strict;
 use warnings;
 
-use Data::MARC::Field008::Utils qw(check_item_form check_music_part check_target_audience);
+use Data::MARC::Field008::Utils qw(check_item_form check_music_composition_form
+	check_music_part check_target_audience);
 use Mo qw(build is);
 use Mo::utils 0.22 qw(check_length_fix check_required);
 
@@ -52,7 +53,7 @@ sub BUILD {
 	# TODO
 
 	# Check 'form_of_composition'.
-	# TODO
+	check_music_composition_form($self, 'form_of_composition');
 
 	# Check 'form_of_item'.
 	check_item_form($self, 'form_of_item');
